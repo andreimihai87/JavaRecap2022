687a00ea80e9772f98d66778fec2145a26a20ae2
andreimihai87 <andreidocleanu@yahoo.com>
2022-07-22 14:33:51 +0300

    COP - For Loops

---
+++ src/amd/caveofprogramming/fl/ForLoops.java
@@ -0,0 +1,22 @@
+package amd.caveofprogramming.fl;
+
+public class ForLoops {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.println("i:" + i);
+		}
+		System.out.println();
+		for(int i = 7; i > 2; i--) {
+			System.out.println("i:" + i);
+		}
+		
+		System.out.println();
+		for(int i = 3; i < 12; i++) {
+			System.out.println("i:" + i);
+		}
+		
+	}
+	
+}
