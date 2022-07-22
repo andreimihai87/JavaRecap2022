7b08d6a569e71d26c0c4a20473ee2ef5acfbae50
andreimihai87 <andreidocleanu@yahoo.com>
2022-07-22 14:37:30 +0300

    COP - Boolean

---
+++ src/amd/caveofprogramming/bool/Boolean.java
@@ -0,0 +1,20 @@
+package amd.caveofprogramming.bool;
+
+public class Boolean {
+
+	public static void main(String[] args) {
+
+		boolean condition1 = false;
+		System.out.println("Condition 1: " + condition1);
+		
+		// less than
+		boolean condition2 = 4 < 5;
+		System.out.println("Condition 2: " + condition2);
+		
+		// greater than
+		boolean condition3 = 2 > 8;
+		System.out.println("Condition 3: " + condition3);
+		
+	}
+
+}
