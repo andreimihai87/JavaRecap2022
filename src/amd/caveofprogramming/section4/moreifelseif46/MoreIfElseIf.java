b5160494972f9e0a7241c3493bfd077f868c0d6a
andreimihai87 <andreidocleanu@yahoo.com>
2022-07-22 21:49:20 +0300

    COP - More If Else If  - changes

--- src/amd/caveofprogramming/section4/moreifelseif46/MoreIfElseIf.java
+++ src/amd/caveofprogramming/section4/moreifelseif46/MoreIfElseIf.java
@@ -10,7 +10,8 @@ public class MoreIfElseIf {
 		System.out.println("Menu");
 		System.out.println("====");
 		System.out.println("1. Print 'Hello!'");
-		System.out.println("2. Exit program");
+		System.out.println("2. How are you?");
+		System.out.println("3. Exit program");
 		System.out.println();
 		System.out.print("Enter an option: ");
 		double input = scanner.nextInt();
@@ -18,7 +19,13 @@ public class MoreIfElseIf {
 		if (input == 1) {
 			System.out.println("Hello!");
 		} else if (input == 2) {
+			System.out.println("How are you?");
+		} else if (input == 3) {
 			System.out.println("Exiting ...");
+			System.exit(0);
+			System.out.println("Doesn't reach this code!");
+		} else if (input > 100) {
+			System.out.println("You've found the hidden Easter Egg!");
 		} else {
 			System.out.println("Wrong Option!");
 		}
