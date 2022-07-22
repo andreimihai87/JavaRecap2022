0e1cd1616a0c264d8e681b47e88a22fa162431d4
andreimihai87 <andreidocleanu@yahoo.com>
2022-07-22 16:00:40 +0300

    COP - Else If

---
+++ src/amd/caveofprogramming/elseif/ElseIf.java
@@ -0,0 +1,29 @@
+package amd.caveofprogramming.elseif;
+
+import java.util.Scanner;
+
+public class ElseIf {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter the room temperature in F: ");
+
+		double temperatureFahrenheit = scanner.nextFloat();
+
+		double temperatureCelsius = (temperatureFahrenheit - 31) * (5.0 / 9.0);
+
+		System.out.printf("Celsius temperature is: %.2f ", temperatureCelsius);
+
+		scanner.close();
+
+		if (temperatureCelsius < 21) {
+			System.out.println("Too cold");
+		} else {
+			System.out.println("Warm enough");
+		}
+
+	}
+
+}
