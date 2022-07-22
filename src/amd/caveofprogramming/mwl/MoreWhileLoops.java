ad95bc67eb54b364338679c7651a17e490e18687
andreimihai87 <andreidocleanu@yahoo.com>
2022-07-22 14:01:29 +0300

    COP - More While Loops

---
+++ src/amd/caveofprogramming/mwl/MoreWhileLoops.java
@@ -0,0 +1,28 @@
+package amd.caveofprogramming.mwl;
+
+public class MoreWhileLoops {
+	public static void main(String[] args) {
+
+		int count = 0;
+		// increment
+		while (count < 10) {
+			System.out.println("Count is " + count);
+			count++;
+		}
+		
+		// decrement
+		while (count > -10) {
+			System.out.println("Count is " + count);
+			count--;
+		}
+
+		// specific output 7, 6, 5, 4, 3
+		count = 7;
+		while (count >= 3) {
+			System.out.printf("Count is %d \n", count);
+			count--;
+		}
+
+	}
+
+}
