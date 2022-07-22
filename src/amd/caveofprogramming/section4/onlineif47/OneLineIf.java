f4ef08028e9efc1633e6814f58e7fb516965ccc1
andreimihai87 <andreidocleanu@yahoo.com>
2022-07-22 23:25:12 +0300

    COP - One line if

---
+++ src/amd/caveofprogramming/section4/onlineif47/OneLineIf.java
@@ -0,0 +1,16 @@
+package amd.caveofprogramming.section4.onlineif47;
+
+public class OneLineIf {
+
+	public static void main(String[] args) {
+
+		boolean isRaining = true;
+
+		if (isRaining)
+			System.out.println("It is raining");
+		else
+			System.out.println("It is not raining");
+
+	}
+
+}
