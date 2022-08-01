5c3824c04c9e1dcbe82b1ebd0d2476bb581fbc68
andreimihai87 <andreidocleanu@yahoo.com>
2022-08-01 23:16:46 +0300

    COP - Checking Passwords

---
+++ src/amd/caveofprogramming/section4/checkPass54/CheckingPasswords.java
@@ -0,0 +1,25 @@
+package amd.caveofprogramming.section4.checkPass54;
+
+import java.util.Scanner;
+
+public class CheckingPasswords {
+	public static void main(String[] args) {
+
+		final String CURRENT_PASSWORD = "Pass1.";
+
+		Scanner scanner = new Scanner(System.in);
+		String enteredPassword;
+
+		while (true) {
+			System.out.print("Please enter the password: ");
+			enteredPassword = scanner.nextLine();
+			if (enteredPassword.equals(CURRENT_PASSWORD)) {
+				System.out.println("Access granted!");
+				break;
+			}
+			System.out.print("Access denied, try again.\n");
+		}
+		scanner.close();
+
+	}
+}
