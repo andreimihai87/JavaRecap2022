8d28d8121a3c0472ad5ad2246e0392831806c17a
andreimihai87 <andreidocleanu@yahoo.com>
2022-08-15 12:22:08 +0300

    COP - Arrays of Strings

---
+++ src/amd/caveofprogramming/section5/arrstrings68/ArraysOfStrings.java
@@ -0,0 +1,19 @@
+package amd.caveofprogramming.section5.arrstrings68;
+
+public class ArraysOfStrings {
+
+	public static void main(String[] args) {
+
+		String[] shopingList = { "Potatoes", "Rice", "Pasta", "Bread" };
+
+		System.out.println(shopingList[1]);
+		System.out.println(shopingList[3]);
+
+		shopingList[1] = "Tomatoes";
+
+		System.out.println(shopingList[1]);
+		System.out.println(shopingList[3]);
+
+	}
+
+}
