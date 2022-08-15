98df12bfd0b407bd789c0d0245aee0d31d538309
andreimihai87 <andreidocleanu@yahoo.com>
2022-08-15 14:39:04 +0300

    COP - For each

---
+++ src/amd/caveofprogramming/section5/fore73/ForEach.java
@@ -0,0 +1,15 @@
+package amd.caveofprogramming.section5.fore73;
+
+public class ForEach {
+
+	public static void main(String[] args) {
+
+		String[] animals = { "Cat", "Dog", "Sloth", "Tiger", "Mouse" };
+		
+		for (String animal : animals) {
+			System.out.println(animal);
+		}
+		
+	}
+
+}
