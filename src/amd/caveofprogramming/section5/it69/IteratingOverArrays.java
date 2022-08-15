3635661aa5462f8b4edb60187cf6c369fc26e305
andreimihai87 <andreidocleanu@yahoo.com>
2022-08-15 13:38:14 +0300

    COP - Iterating over arrays

---
+++ src/amd/caveofprogramming/section5/it69/IteratingOverArrays.java
@@ -0,0 +1,15 @@
+package amd.caveofprogramming.section5.it69;
+
+public class IteratingOverArrays {
+
+	public static void main(String[] args) {
+
+		String[] animals = { "Cat", "Dog", "Sloth" };
+
+		for (int i = 0; i < animals.length; i++) {
+			System.out.println(animals[i]);
+		}
+
+	}
+
+}
