2b2e030bf53b4d6409c44e70957cf57446862621
andreimihai87 <andreidocleanu@yahoo.com>
2022-08-15 12:16:34 +0300

    COP - Arrays

---
+++ src/amd/caveofprogramming/section5/arrays67/Arrays.java
@@ -0,0 +1,16 @@
+package amd.caveofprogramming.section5.arrays67;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		
+		int[] numbers = {2, 4, 6, 8};
+		
+		System.out.println(numbers[0]);
+		System.out.println(numbers[1]);
+		System.out.println(numbers[2]);
+		System.out.println(numbers[3]);
+		
+	}
+
+}
