b9e9d03d6ec77d5d73e3f1987fd0343785f563ca
andreimihai87 <andreidocleanu@yahoo.com>
2022-08-16 16:27:41 +0300

    COP - Getters and setters

---
+++ src/amd/caveofprogramming/section6/gs92/GettersAndSetters.java
@@ -0,0 +1,16 @@
+package amd.caveofprogramming.section6.gs92;
+
+public class GettersAndSetters {
+
+	public static void main(String[] args) {
+
+		Person person1 = new Person();
+		person1.setName("Joe");
+		person1.setAge(35);
+		
+		System.out.println(person1.getName());
+		System.out.println(person1.getAge());
+		
+	}
+
+}
---
+++ src/amd/caveofprogramming/section6/gs92/Person.java
@@ -0,0 +1,24 @@
+package amd.caveofprogramming.section6.gs92;
+
+public class Person {
+
+	String name;
+	int age;
+	
+	void setName(String theName) {
+		name = theName;
+	}
+	
+	void setAge(int theAge) {
+		age = theAge;
+	}
+	
+	String getName() {
+		return name;
+	}
+	
+	int getAge() {
+		return age;
+	}
+	
+}
