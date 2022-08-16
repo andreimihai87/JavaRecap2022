62e937b10a99f5663c6ef6aa3a996c4e7aa09d9e
andreimihai87 <andreidocleanu@yahoo.com>
2022-08-16 15:08:30 +0300

    COP - Return values

---
+++ src/amd/caveofprogramming/section6/rv88/ReturnValues.java
@@ -0,0 +1,15 @@
+package amd.caveofprogramming.section6.rv88;
+
+public class ReturnValues {
+
+	public static void main(String[] args) {
+		
+		Values value1 = new Values();
+		
+		String valueRecived = value1.getValues();
+		
+		System.out.println(valueRecived);
+		
+	}
+	
+}
---
+++ src/amd/caveofprogramming/section6/rv88/Values.java
@@ -0,0 +1,11 @@
+package amd.caveofprogramming.section6.rv88;
+
+public class Values {
+
+	String getValues() {
+		System.out.println("Returning the value...");
+		
+		return "valueX";
+	}
+	
+}
