8069396b4627b338aaeee539b4271079cc12d700
andreimihai87 <andreidocleanu@yahoo.com>
2022-08-16 16:15:46 +0300

    COP - Method parameters

---
+++ src/amd/caveofprogramming/section6/mp90/Calculator.java
@@ -0,0 +1,10 @@
+package amd.caveofprogramming.section6.mp90;
+
+public class Calculator {
+	
+	int square(int value) {
+		System.out.println("Recived the value " + value);
+		return value * value;
+	}
+
+}
---
+++ src/amd/caveofprogramming/section6/mp90/MethodParameters.java
@@ -0,0 +1,14 @@
+package amd.caveofprogramming.section6.mp90;
+
+public class MethodParameters {
+
+	public static void main(String[] args) {
+
+		Calculator calc = new Calculator();
+		int squared = calc.square(8);
+		
+		System.out.println(squared);
+		
+	}
+
+}
