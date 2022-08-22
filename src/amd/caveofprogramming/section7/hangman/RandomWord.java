942d147c0d6e5e0b1ec44f960bb768a3e480e06d
andreimihai87 <andreidocleanu@yahoo.com>
2022-08-22 21:56:39 +0300

    COP - Hangman - Char Arrays

--- src/amd/caveofprogramming/section7/hangman/RandomWord.java
+++ src/amd/caveofprogramming/section7/hangman/RandomWord.java
@@ -5,16 +5,30 @@ import java.util.Random;
 public class RandomWord {
 
 	private String[] words = { "tale", "time", "future", "sister", "sweet", "family", "blacksmith", "gentleman" };
-	private String word;
+	private String chosenWord;
 	private Random random = new Random();
+	private char[] caracters;
 
 	public RandomWord() {
 		int wordIndex = random.nextInt(words.length);
-		word = words[wordIndex];
+		chosenWord = words[wordIndex];
+		caracters = new char[chosenWord.length()];
 	}
 
 	public String toString() {
-		return word;
+
+		String text = "";
+		
+		for (int i = 0; i < caracters.length; i++) {
+			if (caracters[i] == '\u0000') {
+				text += "_";
+			} else {
+				text += caracters[i];
+			}
+			text += " ";
+		}
+
+		return text;
 	}
 
 }
