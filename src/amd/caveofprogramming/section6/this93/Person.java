0bc9c9f358ec5ec1dc5dfe7b4693f23aa57e7c78
andreimihai87 <andreidocleanu@yahoo.com>
2022-08-22 14:31:43 +0300

    COP - This

---
+++ src/amd/caveofprogramming/section6/this93/Person.java
@@ -0,0 +1,24 @@
+package amd.caveofprogramming.section6.this93;
+
+public class Person {
+
+	String name;
+	int age;
+	
+	void setName(String name) {
+		this.name = name;
+	}
+	
+	void setAge(int age) {
+		this.age = age;
+	}
+	
+	String getName() {
+		return name;
+	}
+	
+	int getAge() {
+		return age;
+	}
+	
+}
