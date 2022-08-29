e2ebc11ce7cdf4b1dcfb631a9484cc3b533b3fcc
andreimihai87 <andreidocleanu@yahoo.com>
2022-08-29 17:55:14 +0300

    COP - Section 10 - Final Classes

---
+++ src/amd/caveofprogramming/section10/fc130/FinalClasses.java
@@ -0,0 +1,20 @@
+package amd.caveofprogramming.section10.fc130;
+
+final class Person {
+
+}
+
+/*
+ * Can't extend the final class Person 
+ * class Employee extends Person {
+ * 
+ * }
+ */
+
+public class FinalClasses {
+
+	public static void main(String[] args) {
+
+	}
+
+}
