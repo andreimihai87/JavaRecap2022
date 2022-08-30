a3d7e4bcfd4b28565493ebeab67c2f8e8777f972
andreimihai87 <andreidocleanu@yahoo.com>
2022-08-30 14:30:48 +0300

    COP - Section 11 - RPS - game loop and announcing the winner

--- src/amd/caveofprogramming/section11/game/Game.java
+++ src/amd/caveofprogramming/section11/game/Game.java
@@ -8,27 +8,45 @@ public class Game {
 	private GameItem[] gameItems = { new Rock(), new Paper(), new Scissors() };
 
 	public void run() {
-		String[] results = { "Won", "Draw", "Lose" }; 
+		int[] score = { 0, 0 };
+		String[] results = { "won", "draw", "lose" };
 
-		System.out.println("Options: 1. Rock , 2. Paper, 3. Scissors");
-		System.out.print("Please choose an option: ");
-		
-		// user choice
 		Scanner myScanner = new Scanner(System.in);
-		int index1 = myScanner.nextInt();
 
-		// computer choice
-		Random randomNr = new Random();
-		int index2 = randomNr.nextInt(gameItems.length);
-		
-		GameItem game1 = gameItems[index1 - 1];
-		GameItem game2 = gameItems[index2];
-		
-		System.out.println("Your choice:\t " + game1.getName());
-		System.out.println("Computer choice: " + game2.getName());
-		System.out.println("You " + results[1 - game1.compareTo(game2)]);
+		for (int i = 0; i < 3; i++) {
+			System.out.println("Options: 1. Rock , 2. Paper, 3. Scissors");
+			System.out.print("Please choose an option: ");
 
+			// user choice
+			int index1 = myScanner.nextInt();
+
+			// computer choice
+			Random randomNr = new Random();
+			int index2 = randomNr.nextInt(gameItems.length);
+
+			GameItem game1 = gameItems[index1 - 1];
+			GameItem game2 = gameItems[index2];
+
+			int resultIndex = 1 - game1.compareTo(game2);
+			if (resultIndex == 0) {
+				score[0]++;
+			} else if (resultIndex == 2) {
+				score[1]++;
+			}
+			System.out.println("Your choice:\t " + game1.getName());
+			System.out.println("Computer choice: " + game2.getName());
+			System.out.printf("You %s round %d \n\n", results[resultIndex], i + 1);
+		}
 		myScanner.close();
+		
+		System.out.printf("The final score is: You: %d - Computer: %d\n", score[0], score[1]);
+		if (score[0] > score[1]) {
+			System.out.println("You won the game!");
+		} else if (score[0] == score[1]) {
+			System.out.println("You draw the game!");
+		} else {
+			System.out.println("You lost the game!");
+		}
 
 	}
 
