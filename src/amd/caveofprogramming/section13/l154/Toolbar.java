817f83eaa78039204e57c0d0ed17156895d9ba94
andreimihai87 <andreidocleanu@yahoo.com>
2022-09-01 21:58:11 +0300

    COP - Section 13 - Toolbars

---
+++ src/amd/caveofprogramming/section13/l154/Toolbar.java
@@ -0,0 +1,13 @@
+package amd.caveofprogramming.section13.l154;
+
+import javax.swing.JButton;
+import javax.swing.JToolBar;
+
+public class Toolbar extends JToolBar {
+	
+	public Toolbar() {
+		add(new JButton("YES"));
+		add(new JButton("NO"));
+	}
+
+}
