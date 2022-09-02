8b3cff948d0c3cfa11e99362a988f6568cc5b9b1
andreimihai87 <andreidocleanu@yahoo.com>
2022-09-02 13:29:02 +0300

    COP - Section 14 - Main Method

---
+++ src/amd/caveofprogramming/section14/l159/MainMethod.java
@@ -0,0 +1,15 @@
+package amd.caveofprogramming.section14.l159;
+
+public class MainMethod {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello!");
+
+		for (String arg : args) {
+			System.out.println(arg);
+		}
+
+	}
+
+}
