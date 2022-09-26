e5bf8ad159f54fadf9233be656cd777ce33436b5
andreimihai87 <andreidocleanu@yahoo.com>
2022-09-26 11:02:45 +0300

    COP - Section 15 - Button Clicks

---
+++ src/amd/caveofprogramming/section15/l174/MainPanel.java
@@ -0,0 +1,13 @@
+package amd.caveofprogramming.section15.l174;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class MainPanel extends JPanel{
+	
+	public MainPanel(Color color) {
+		setBackground(color);
+	}
+
+}
