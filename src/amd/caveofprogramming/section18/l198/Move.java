440b78c32cc00e85213133b8255d90ad8d2a9cb3
andreimihai87 <andreidocleanu@yahoo.com>
2022-10-03 17:29:33 +0300

    COP - Section 18 - Improving Rock Paper Scissors

---
+++ src/amd/caveofprogramming/section18/l198/Move.java
@@ -0,0 +1,40 @@
+package amd.caveofprogramming.section18.l198;
+
+public enum Move {
+
+	ROCK(0, "Rock"), PAPER(1, "Paper"), SCISSORS(2, "Scissors");
+
+	int id;
+	String name;
+	Move beats;
+	
+	Move(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	static {
+		ROCK.beats = SCISSORS;
+		PAPER.beats = ROCK;
+		SCISSORS.beats = PAPER;
+	}
+	
+	boolean beats(Move move) {
+		return true;
+	}
+	
+	// @formatter:off
+	private int[][] comparison = {
+			//				rock 	 paper	 scissors
+			/* rock */		{ 0 , 	  -1,		 1 },
+			/* paper */		{ 1 ,  	   0, 		-1 },
+			/* scissors */	{-1 , 	   1, 		 0 }
+	};
+	// @formatter:on
+	
+	/** Compare to Move */
+	public int compare(Move otherItem) {
+		return comparison[id][otherItem.id];
+	}
+
+}
---
+++ src/amd/caveofprogramming/section18/l198/RPS.java
@@ -0,0 +1,17 @@
+package amd.caveofprogramming.section18.l198;
+
+/**
+ * Rock Paper Scissors
+ * @author andrew
+ *
+ */
+public class RPS {
+
+	public static void main(String[] args) {
+
+		Game game = new Game();
+		game.run();
+
+	}
+
+}
