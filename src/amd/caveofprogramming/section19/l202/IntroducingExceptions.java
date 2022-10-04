c8cecfc4eae44d17a10f4eb00f7b9e0e7616fd5a
andreimihai87 <andreidocleanu@yahoo.com>
2022-10-04 15:11:25 +0300

    COP - Section 19 - Introducing Exceptions

---
+++ src/amd/caveofprogramming/section19/l202/IntroducingExceptions.java
@@ -0,0 +1,13 @@
+package amd.caveofprogramming.section19.l202;
+
+public class IntroducingExceptions {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		System.out.println("One");
+		Thread.sleep(3000);
+		System.out.println("Two");
+		
+	}
+
+}
