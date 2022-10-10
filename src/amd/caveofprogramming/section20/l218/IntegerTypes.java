b02120e803a8cc6176bfc971d1b2059a6046058a
andreimihai87 <andreidocleanu@yahoo.com>
2022-10-10 16:13:30 +0300

    COP - Section 20 - Integer Types

---
+++ src/amd/caveofprogramming/section20/l218/IntegerTypes.java
@@ -0,0 +1,17 @@
+package amd.caveofprogramming.section20.l218;
+
+public class IntegerTypes {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+
+		// byte, short, integer, long
+
+		byte byteValue = 12;
+		short shortValue = 12000;
+		int intValue = 12000000;
+		long longValue = 12000000000L;
+		
+	}
+
+}
