c85ac02e8459c1e2eefbc948a214f811993f985a
andreimihai87 <andreidocleanu@yahoo.com>
2022-10-10 17:02:20 +0300

    COP - Section 20 -  Class Equivalents of Primitive Types

---
+++ src/amd/caveofprogramming/section20/l221/ClassEquivPrimitiveTypes.java
@@ -0,0 +1,28 @@
+package amd.caveofprogramming.section20.l221;
+
+public class ClassEquivPrimitiveTypes {
+
+	public static void main(String[] args) {
+
+		// Primitive Types - Equivalent Classes
+		/*
+		 * byte		- 	Byte
+		 * short 	- 	Short
+		 * int 		- 	Integer
+		 * long		- 	Long
+		 * 
+		 * float 	- 	Float
+		 * double 	- 	Double
+		 * 
+		 * char 	- 	Character
+		 * boolean	- 	Boolean
+		 */
+		
+		Integer intValue = 128;
+		Integer intValue2 = 128;
+		
+		System.out.println(intValue.equals(intValue2));
+		
+	}
+
+}
