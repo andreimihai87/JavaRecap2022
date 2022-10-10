e0ea96033e9fa15fb407b8ddf8e10cd8c9a98e8e
andreimihai87 <andreidocleanu@yahoo.com>
2022-10-10 12:01:33 +0300

    COP - Section 19 - NullPointerException

---
+++ src/amd/caveofprogramming/section19/l212/App.java
@@ -0,0 +1,17 @@
+package amd.caveofprogramming.section19.l212;
+
+import java.util.Scanner;
+
+public class App {
+	
+	public static void main(String[] args) {
+
+//		Scanner scanner1 = null;
+//		scanner1.nextInt();
+		
+		String name = null;
+		name.length();
+		
+	}
+
+}
