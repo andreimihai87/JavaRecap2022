81ba2938d6c528eeb3831a36a47362d811a4b04f
andreimihai87 <andreidocleanu@yahoo.com>
2022-10-12 14:06:10 +0300

    COP - Section 21 - GOL - Calculating Margins

--- src/amd/caveofprogramming/section21/l226tol246/gui/GamePanel.java
+++ src/amd/caveofprogramming/section21/l226tol246/gui/GamePanel.java
@@ -9,18 +9,34 @@ import javax.swing.JPanel;
 public class GamePanel extends JPanel {
 	
 	private static final long serialVersionUID = 1L;
-
+	
+	private static final int CELL_SIZE = 50;
+	
+	private static final Color backgroundColor = Color.BLACK;
+	private static final Color foregroundColor = Color.GREEN;
+	private static final Color cellColor = Color.GRAY;
+	
+	private int leftRightMargin;
+	private int topBottomMargin;
+	
 	public GamePanel() {
-		setBackground(Color.GREEN);
 	}
 	
 	@Override
 	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
+		
+		int width = getWidth();
+		int height = getHeight();
+		
+		leftRightMargin = ((width & CELL_SIZE) + CELL_SIZE) / 2;
+		topBottomMargin = ((height & CELL_SIZE) + CELL_SIZE) / 2;
 		
 		Graphics2D g2 = (Graphics2D) g;
-		g2.setColor(Color.RED);
-		g2.drawRect(0, 0, 20, 20);
+		g2.setColor(backgroundColor);
+		g2.drawRect(0, 0, width, height);
+		
+		g2.setColor(foregroundColor);
+		g2.drawRect(leftRightMargin, topBottomMargin, width - 2 * leftRightMargin , height - 2 * topBottomMargin);
 		
 	}
 
