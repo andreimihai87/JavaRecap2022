23a1d6b11d9019decc92afbb89926c0d65fc02b5
andreimihai87 <andreidocleanu@yahoo.com>
2022-10-13 13:45:02 +0300

    COP - Section 21 - GOL - Handling Key Presses

--- src/amd/caveofprogramming/section21/l226tol246/gui/MainFrame.java
+++ src/amd/caveofprogramming/section21/l226tol246/gui/MainFrame.java
@@ -1,6 +1,9 @@
 package amd.caveofprogramming.section21.l226tol246.gui;
 
 import java.awt.BorderLayout;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 import javax.swing.JFrame;
 
 public class MainFrame extends JFrame {
@@ -15,6 +18,26 @@ public class MainFrame extends JFrame {
 		setLayout(new BorderLayout());
 		add(mainPanel, BorderLayout.CENTER);
 		
+		addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				int keyCode = e.getKeyCode();
+				switch (keyCode) {
+				case 8:
+					System.out.println("back space");
+					break;
+				case 10:
+					System.out.println("enter");
+					break;
+				case 32:
+					System.out.println("space");
+					break;
+				default:
+					break;
+				}
+			}
+		});
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(800, 600);
 		setVisible(true);
