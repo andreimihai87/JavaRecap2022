52f9a067bcb7d2b88d71f48f5df8443491bf8fc7
andreimihai87 <andreidocleanu@yahoo.com>
2022-11-02 20:43:51 +0200

    COP - Section 24 - A Simple Hierarchy

---
+++ src/amd/caveofprogramming/section24/l271/App.java
@@ -0,0 +1,14 @@
+package amd.caveofprogramming.section24.l271;
+
+import amd.caveofprogramming.section24.l271.hierarchy.Cat;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Cat cat1 = new Cat("Tom");
+		System.out.println(cat1);
+		
+	}
+
+}
---
+++ src/amd/caveofprogramming/section24/l271/hierarchy/Mammal.java
@@ -0,0 +1,9 @@
+package amd.caveofprogramming.section24.l271.hierarchy;
+
+public class Mammal extends Creature {
+	
+	public Mammal(String name) {
+		super(name);
+	}
+	
+}
