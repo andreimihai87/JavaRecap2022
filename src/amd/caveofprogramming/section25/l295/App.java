2c7d93f2d1d5779f616196487167a8306bdcca93
andreimihai87 <andreidocleanu@yahoo.com>
2022-11-04 16:58:23 +0200

    COP - Section 25 - Static Methods in Interfaces

---
+++ src/amd/caveofprogramming/section25/l295/App.java
@@ -0,0 +1,11 @@
+package amd.caveofprogramming.section25.l295;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		ITest.run();
+		
+	}
+
+}
---
+++ src/amd/caveofprogramming/section25/l295/ITest.java
@@ -0,0 +1,9 @@
+package amd.caveofprogramming.section25.l295;
+
+public interface ITest {
+	
+	static void run() {
+		System.out.println("run");
+	}
+
+}
