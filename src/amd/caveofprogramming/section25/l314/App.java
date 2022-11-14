4e383d3979e78d251f74d2469c49ae3c9ebce1c4
andreimihai87 <andreidocleanu@yahoo.com>
2022-11-14 14:37:08 +0200

    COP - Section 25 - Hashing Algorithms

---
+++ src/amd/caveofprogramming/section25/l314/App.java
@@ -0,0 +1,28 @@
+package amd.caveofprogramming.section25.l314;
+
+class Creature {
+    
+    private int id;
+    private String name;
+    
+    public Creature(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    
+    @Override
+    public String toString() {
+        return id + ": " + name;
+    }
+    
+}
+
+public class App {
+
+    public static void main(String[] args) {
+
+        
+        
+    }
+
+}
