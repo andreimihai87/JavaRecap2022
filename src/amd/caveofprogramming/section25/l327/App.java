faec2b9ea1f74011afa40f60a5b6b6729beabe8a
andreimihai87 <andreidocleanu@yahoo.com>
2022-11-15 22:27:08 +0200

    COP - Section 25 - Static Imports

---
+++ src/amd/caveofprogramming/section25/l327/App.java
@@ -0,0 +1,19 @@
+package amd.caveofprogramming.section25.l327;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.E;
+import static java.lang.Math.abs;
+//or
+// import static java.lang.Math.*;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println(PI);
+        System.out.println(abs(-8));
+        System.out.println(E);
+        
+    }
+
+}
