7e60ef942fb9bb8bc43d194b3fe16af6d6d7d1ac
andreimihai87 <andreidocleanu@yahoo.com>
2022-11-24 23:39:09 +0200

    COP - Section 30 - Group By

---
+++ src/amd/caveofprogramming/section30/l396/App.java
@@ -0,0 +1,42 @@
+package amd.caveofprogramming.section30.l396;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+// Using streams:
+// Load a text file.
+// Create a map of all the words in the text file,
+// where the keys in the map are words
+// and the values are a count of the number of times each word occurs in the
+// file.
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+
+        // @formatter:off
+        var mapWords = Files.lines(Paths.get("1984.txt"))
+                .map(s -> s.split("[^a-zA-Z]"))
+                .map(list -> Arrays.asList(list))
+                .flatMap(list -> list.stream())
+                .filter(s -> !s.isBlank())
+                .map(String::toLowerCase)
+                .collect(Collectors.groupingBy(word -> word, Collectors.counting()));
+        // @formatter:on
+
+        var list = mapWords.entrySet().stream().sorted((v1, v2) -> v2.getValue().compareTo(v1.getValue()))
+                .collect(Collectors.toList());
+
+        for (int i = 0; i < list.size(); i++) {
+            if (i % 7 == 0) {
+                System.out.println();
+            }
+            System.out.printf("%-11s : %4d | ", list.get(i).getKey(), list.get(i).getValue());
+        }
+
+    }
+
+}
