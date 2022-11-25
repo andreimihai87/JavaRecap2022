8a1539764c20e71c8e616879be8be2245c5755dd
andreimihai87 <andreidocleanu@yahoo.com>
2022-11-25 12:54:44 +0200

    COP - Section 32 - Formatting Binary Numbers

---
+++ src/amd/caveofprogramming/section32/l407/App.java
@@ -0,0 +1,17 @@
+package amd.caveofprogramming.section32.l407;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        int value = 0b100100;
+        
+        System.out.println(toBinary(value));
+
+    }
+
+    private static String toBinary(int value) {
+        return String.format("%8s", Integer.toBinaryString(value).replace(" ", "0"));
+    }
+
+}
